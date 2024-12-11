f2bfaa36529cac1ab9b96766bf6bf3bf721d48ec
abhishek verma <abhi.av017@gmail.com>
2024-12-11 18:53:54 +0530

    Get Profile by username error solved

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,6 @@ function Profile() {
     const [profile, setProfile] = useState(null);
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-    console.log(username)
     useEffect(() => {
         const fetchProfile = async () => {
             setLoading(true);
@@ -23,29 +22,31 @@ function Profile() {
             fetchProfile();
         }
     }, [username]);
-    return (
-        <div className="bg-zinc-900 profile flex flex-col gap-6 justify-center items-center h-[88.1vh]">
-            <div className='flex justify-center flex-col text-white bg-zinc-950 border-2 border-zinc-700 rounded-xl py-3 px-2 my-1 shadow-lg shadow-white/20'>
-                <h1 className='text-center font-bold text-xl text-white'>Your Profile!!!</h1>
-                <div className='flex justify-between gap-4 p-4'>
-                    <img className='rounded-full inline-block w-1/3 h-[90%]' src={profile.account.avatar.url} alt='' />
-                    <div className='flex flex-col justify-center'>
-                        <div className='flex justify-between gap-4'>
-                            <span className='inline-block text-xl font-bold underline m-2'>{profile.account.username}</span>
-                            <button className='border-zinc-200 hover:bg-zinc-200 hover:text-black text-white border-2 rounded-2xl px-2 py-1'>Follow</button>
+    if(profile){
+
+        return (
+            <div className="bg-zinc-900 profile flex flex-col gap-6 justify-center items-center h-[88.1vh]">
+                <div className='flex justify-center flex-col text-white bg-zinc-950 border-2 border-zinc-700 rounded-xl py-3 px-2 my-1 shadow-lg shadow-white/20'>
+                    <div className='flex justify-between gap-4 p-4'>
+                        <img className='rounded-full inline-block w-1/3 h-full' src={profile.account.avatar.url} alt='' />
+                        <div className='flex flex-col justify-center'>
+                            <div className='flex justify-between gap-4'>
+                                <span className='inline-block text-xl font-bold underline m-2'>{profile.account.username}</span>
+                                <button className='border-zinc-200 hover:bg-zinc-200 hover:text-black text-white border-2 rounded-2xl px-2 py-1'>Follow</button>
+                            </div>
+                            <div>
+                                <span className='inline-block text-xl font-bold underline m-2'>Followers: {profile.followersCount}</span>
+                                <span className='inline-block text-xl font-bold underline m-2'>Following: {profile.followingCount}</span>
+                            </div>
+                            <span className='inline-block text-xl font-bold underline m-2'>{profile.bio}</span>
                         </div>
-                        <div>
-                            <span className='inline-block text-xl font-bold underline m-2'>Followers: {profile.followersCount}</span>
-                            <span className='inline-block text-xl font-bold underline m-2'>Following: {profile.followingCount}</span>
-                        </div>
-                        <span className='inline-block text-xl font-bold underline m-2'>{profile.bio}</span>
+                    </div>
+                    <div className='flex flex-col items-start mx-4 w-1/2'>
                     </div>
                 </div>
-                <div className='flex flex-col items-start mx-4 w-1/2'>
-                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Profile
